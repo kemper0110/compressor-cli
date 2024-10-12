d43477d489681a4413256c67d69b4eec1e868675
Danil <danil2002.2043@gmail.com>
2024-10-13 00:59:15 +0300

    initial

--- main.cpp
+++ main.cpp
@@ -6,19 +6,14 @@ int main() {
     std::ifstream ifs("../mumu.txt");
     if (!ifs.is_open())
         throw std::runtime_error("File not found");
-    ifs.seekg(0, std::ios::end);
-    const auto size = ifs.tellg();
-    ifs.seekg(0, std::ios::beg);
-    std::cout << "initial size is: " << size << '\n';
 
-    std::stringstream pss, oss;
-    Huffman::HuffmanCompressor().compress(ifs, pss);
-    LZSS::LZSSCompressor(256, 2048).compress(pss, oss);
+    std::stringstream iss, pss, oss;
+    iss << ifs.rdbuf();
 
-    std::string compress_result = oss.str();
-    std::cout << "compressed size: " << compress_result.size() << '\n';
+    const auto start = std::chrono::steady_clock::now();
+    Huffman::HuffmanCompressor().compress(iss, pss);
+    LZSS::LZSSCompressor(1024, 4096).compress(pss, oss);
+    const auto end = std::chrono::steady_clock::now();
 
-    const auto reduce = (double) compress_result.size() / size - 1.0;
-    std::cout << reduce * 100 << "% file size lost" << '\n';
-    // expected loss: ~35%
+    std::cout << "compress time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n";
 }
